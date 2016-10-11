0bf0f5dc5987fcb3badd068c1a7bcd1d0c19cd40
Theo Judas <theo_jd@hotmail.fr>
2016-10-10 22:53:24 -0400

    Menu Upagrade

---
+++ Hibana/Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour {
+
+	public void LoadGame()
+    {
+//        SceneManager.LoadScene("scene1");
+    }
+
+    public void LoadBegin()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    public void LoadTutorial()
+    {
+//        SceneManager.LoadScene("tuto");
+    }
+
+    public void LoadSynopsis()
+    {
+//        SceneManager.LoadScene("synopsis");
+    }
+
+    public void LoadCredits()
+    {
+//        SceneManager.LoadScene("credits");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
