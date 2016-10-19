a467a601e7a9c3fe6a45767239756535561a7ef3
Theo Judas <theo_jd@hotmail.fr>
2016-10-19 15:05:40 -0400

    HUD upgraded with environnemental variables

--- Hibana/Assets/Scripts/FloraAndFauna.cs
+++ Hibana/Assets/Scripts/FloraAndFauna.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FloraAndFauna : MonoBehaviour {
     public int _numberOfTreesBurned = 0;
     public int _numberOfBeastsKilled = 0;
-	
+    public Text _treeText;
+    public Text _beastText;
     public void TreeBurned()
     {
         ++_numberOfTreesBurned;
-        Debug.Log("Tree burned " + _numberOfTreesBurned);
+        _treeText.text = _numberOfTreesBurned.ToString();
+//        Debug.Log("Tree burned " + _numberOfTreesBurned);
     }
 
     public void BeastKilled()
     {
         ++_numberOfBeastsKilled;
-        Debug.Log("Beast Killed " + _numberOfBeastsKilled);
+        _beastText.text = _numberOfBeastsKilled.ToString();
+//        Debug.Log("Beast Killed " + _numberOfBeastsKilled);
     }
 
     public int FloraAndFaunaDestuctionIndex()
