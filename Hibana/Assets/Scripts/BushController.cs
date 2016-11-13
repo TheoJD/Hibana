fcb4894002147d471605af53c3d2e334175c7bd8
Theo Judas <theo_jd@hotmail.fr>
2016-11-13 15:03:26 -0500

    Adding bushes who can stinged the player

---
+++ Hibana/Assets/Scripts/BushController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BushController : MonoBehaviour {
+    private const int _damages = 5;
+    private const float _timeBetweenDamages = 1.5f;
+    private bool _canSting = true;
+    private Coroutine _coroutine;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (_canSting && collider.tag == GameManager.GetInstance().GetPlayerTag())
+        {
+            _coroutine = StartCoroutine(Sting());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == GameManager.GetInstance().GetPlayerTag())
+        {
+            StopCoroutine(_coroutine);
+            _canSting = true;
+        }
+    }
+
+    private IEnumerator Sting()
+    {
+        while (true)
+        {
+            _canSting = false;
+            GameManager.GetInstance().TakeDamage(_damages);
+            yield return new WaitForSeconds(_timeBetweenDamages);
+            _canSting = true;
+        }
+    }
+}
