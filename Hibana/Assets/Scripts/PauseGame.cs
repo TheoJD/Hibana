b5df5cdbe65cddb1367e28906ba3367a71604ad1
Theo Judas <theo_jd@hotmail.fr>
2016-12-10 22:13:49 -0500

    Pause issue fixed

--- Hibana/Assets/Scripts/PauseGame.cs
+++ Hibana/Assets/Scripts/PauseGame.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 
 public class PauseGame : MonoBehaviour {
-    private bool inPause;
+    private bool _inPause = false;
+    private bool _canPause = false;
     public Transform canvas;
 	// Use this for initialization
 	void Start () {
-        inPause = false;
+        StartCoroutine(EnablePause());
 	}
+
+    IEnumerator EnablePause()
+    {
+        yield return new WaitForSeconds(2.0f);
+        _canPause = true;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Escape))
+	    if (_canPause && Input.GetKeyDown(KeyCode.Escape))
         {
             Pause();
         }
@@ -20,8 +28,8 @@ public class PauseGame : MonoBehaviour {
     public void Pause()
     {
         GameManager.GetInstance().EnableControl();
-        inPause = !inPause;
-        canvas.gameObject.SetActive(inPause);
-        Time.timeScale = inPause ? 0 : 1;
+        _inPause = !_inPause;
+        canvas.gameObject.SetActive(_inPause);
+        Time.timeScale = _inPause ? 0 : 1;
     }
 }
---
+++ README.md
@@ -0,0 +1,22 @@
+1- COMMANDES
+
+Gauche : Q
+Droite : D
+Saut : Barre espace
+Double saut : Saut puis Barre espace
+Tirer au lance flamme : Clic gauche
+
+2- MECANIQUE DE JEU
+
+Vous incarnez Hibana, une petite fille armé d'un bras mécanique faisant office de lance-flamme.
+Vous devez sortir de la forêt.
+
+Pour avancer, vous devez atteindre le portail de chaque scène : la flamme blanche.
+
+Esquiver ou brûler les ennemis qui n'attaquent qu'au corps à corps.
+
+Attention aux buissons, ils piquent. Les brûler les neutralisent.
+
+Vous trouverez sur le chemin des munitions pour recharger le lance-flamme.
+
+Hibana est insensible aux flammes.
